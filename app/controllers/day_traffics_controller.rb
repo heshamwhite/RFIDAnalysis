ab3de4b2ebf4222443666a06c2ffe47e94cac356
Heshamwhite <heshamwhite@gmail.com>
2015-05-27 16:15:22 +0300

    1 Model + Editing 3 Controlllers

--- app/controllers/day_traffics_controller.rb
+++ app/controllers/day_traffics_controller.rb
@@ -1,9 +1,13 @@
 class DayTrafficsController < ApplicationController
   def index
+
   end
 
   def list
-    @dayTraffics = DayTraffic.all
+    #@dayTraffics = DayTraffic.all
+    @start_date = Date.new(2014,1,1)
+    @end_date = Date.new(2015,1,1)
+    @dayTraffics = DayTraffic.where('traffic_date < ? AND traffic_date > ? ',@end_date,@start_date)
     render json: @dayTraffics
   end
 end
